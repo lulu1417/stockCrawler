785c520817b3b14f58268128bbef3511f854ffc1
lulu1417 <boa1417@gmail.com>
2020-04-14 14:10:22 +0800

    get all data

--- getSingleElementCrawler.php
+++ getSingleElementCrawler.php
@@ -1,8 +1,12 @@
 <?php
 include_once('simple_html_dom.php');
 $urls = [
-    'http://jsjustweb.jihsun.com.tw/z/zc/zcp/zcpa/zcpa0.djhtm?b=Y&a=2330',
-//    'http://jsjustweb.jihsun.com.tw/z/zc/zcp/zcpa/zcpa0.djhtm?b=Q&a=2330'
+    'http://jsjustweb.jihsun.com.tw/z/zc/zcp/zcpa/zcpa0.djhtm?b=Y&a=2330', //資產負債年表
+    'http://jsjustweb.jihsun.com.tw/z/zc/zcp/zcpa/zcpa0.djhtm?b=Q&a=2330', //資產負債季表
+    'http://jsjustweb.jihsun.com.tw/z/zc/zcq/zcq0.djhtm?b=Y&a=2330', //損益年表
+    'http://jsjustweb.jihsun.com.tw/z/zc/zcq/zcq0.djhtm?b=Q&a=2330', //損益季表
+    'http://jsjustweb.jihsun.com.tw/z/zc/zc30.djhtm?b=Y&a=2330', //現金流量年表
+    'http://jsjustweb.jihsun.com.tw/z/zc/zc30.djhtm?b=Q&a=2330' //現金流量表
 ];
 
 foreach ($urls as $url) {
@@ -13,6 +17,7 @@ foreach ($urls as $url) {
     $html = curl_exec($curl);
     $dom = new simple_html_dom(null, true, true, DEFAULT_TARGET_CHARSET, true, DEFAULT_BR_TEXT, DEFAULT_SPAN_TEXT);
     $html = $dom->load($html, true, true);
+    echo "\n";
     foreach ($html->find('td[class=t10]') as $a) {
         $title = $a->plaintext;
         echo($title . "\n");
@@ -34,18 +39,18 @@ foreach ($urls as $url) {
         $j++;
     }
     $j = 0;
+    $row = 1;
     foreach ($html->find('td[class=t4t1]') as $a) {
-        echo($a->plaintext." ");
-        while(isset($content[$j]) && ($j % 7 !== 0 || $j == 0)) {
+        echo($a->plaintext . " ");
+        while (isset($content[$j]) && ($j < 7 * $row + $row - 1) || $j == 0) {
             echo($content[$j] . " ");
             $j++;
         }
         echo($content[$j]);
         $j++;
+        $row++;
         echo "\n";
     }
-
-
 
 
 }
