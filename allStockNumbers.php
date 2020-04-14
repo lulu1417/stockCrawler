29038b8d8405fd9ef3328760568814e06d423546
lulu1417 <boa1417@gmail.com>
2020-04-14 20:00:31 +0800

    correct stock number

--- allStockNumbers.php
+++ allStockNumbers.php
@@ -1,17 +1,14 @@
 <?php
+//include 'init.php';
 
-class allStockNumbers extends initializeCurl
-{
-    function getAllStockNumbers()
-    {
+    $urls = ['https://www.tej.com.tw/webtej/doc/uid.htm'];
 
-        $urls = ['https://www.tej.com.tw/webtej/doc/uid.htm'];
-
-        foreach ($urls as $url) {
-            $html = $this->initCurl($url);
-            $i = 0;
-            foreach ($html->find('tr td[class="xl24"]') as $a) {
-                $number = filter_var($a->plaintext, FILTER_SANITIZE_NUMBER_INT);
+    foreach ($urls as $url) {
+        $html = $curl::initCurl($url);
+        $i = 0;
+        foreach ($html->find('table border="1"') as $a) {
+            foreach ($a->find('tr td[class="xl24"]') as $b){
+                $number = filter_var($b->plaintext, FILTER_SANITIZE_NUMBER_INT);
                 $stockNumbers[$i] = $number;
 //                echo($i . " " . $number . " ");
 //                echo "\n";
@@ -19,6 +16,5 @@ class allStockNumbers extends initializeCurl
             }
 
         }
-        return $stockNumbers;
+
     }
-}
--- getAllContentCrawler.php
+++ getAllContentCrawler.php
@@ -1,34 +1,34 @@
 <?php
 include 'init.php';
 include 'allStockNumbers.php';
-$getNumbers = new AllStockNumbers();
-$stockNumbers = $getNumbers->getAllStockNumbers();
-
 foreach ($stockNumbers as $stockNumber) {
-    $urls = [
-        'http://jsjustweb.jihsun.com.tw/z/zc/zcp/zcpa/zcpa0.djhtm?b=Y&a=' . $stockNumber, //資產負債年表
-        'http://jsjustweb.jihsun.com.tw/z/zc/zcp/zcpa/zcpa0.djhtm?b=Q&a='.$stockNumber, //資產負債季表
-        'http://jsjustweb.jihsun.com.tw/z/zc/zcq/zcq0.djhtm?b=Y&a='.$stockNumber, //損益年表
-        'http://jsjustweb.jihsun.com.tw/z/zc/zcq/zcq0.djhtm?b=Q&a='.$stockNumber, //損益季表
-        'http://jsjustweb.jihsun.com.tw/z/zc/zc30.djhtm?b=Y&a='.$stockNumber, //現金流量年表
-        'http://jsjustweb.jihsun.com.tw/z/zc/zc30.djhtm?b=Q&a='.$stockNumber //現金流量季表
-    ];
+    if($stockNumber !== null){
+        $urls = [
+            'http://jsjustweb.jihsun.com.tw/z/zc/zcp/zcpa/zcpa0.djhtm?b=Y&a=' . $stockNumber, //資產負債年表
+            'http://jsjustweb.jihsun.com.tw/z/zc/zcp/zcpa/zcpa0.djhtm?b=Q&a='.$stockNumber, //資產負債季表
+            'http://jsjustweb.jihsun.com.tw/z/zc/zcq/zcq0.djhtm?b=Y&a='.$stockNumber, //損益年表
+            'http://jsjustweb.jihsun.com.tw/z/zc/zcq/zcq0.djhtm?b=Q&a='.$stockNumber, //損益季表
+            'http://jsjustweb.jihsun.com.tw/z/zc/zc30.djhtm?b=Y&a='.$stockNumber, //現金流量年表
+            'http://jsjustweb.jihsun.com.tw/z/zc/zc30.djhtm?b=Q&a='.$stockNumber //現金流量季表
+        ];
 
 
-    foreach ($urls as $url) {
-        $html = $curl::initCurl($url);
-        $i = 0;
-        foreach ($html->find('tr') as $a) {
-            if ($i > 2) {
-                $arr[$i] = $a->plaintext;
+        foreach ($urls as $url) {
+            $html = $curl::initCurl($url);
+            $i = 0;
+            foreach ($html->find('tr') as $a) {
+                if ($i > 2) {
+                    $arr[$i] = $a->plaintext;
+                }
+                $i++;
             }
-            $i++;
+            foreach($arr as $a) {
+                echo $a . "\n";
+            }
         }
-        foreach ($arr as $a) {
-            echo $a . "\n";
-        }
+
+//        die();
     }
 
-    die();
 }
 
