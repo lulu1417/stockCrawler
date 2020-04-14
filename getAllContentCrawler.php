2ac497dc5517f96d6349df7815989b757af848ae
lulu1417 <boa1417@gmail.com>
2020-04-14 19:09:59 +0800

    cut unnecessary line

--- getAllContentCrawler.php
+++ getAllContentCrawler.php
@@ -3,9 +3,10 @@ include 'init.php';
 include 'allStockNumbers.php';
 $getNumbers = new AllStockNumbers();
 $stockNumbers = $getNumbers->getAllStockNumbers();
-foreach ($stockNumbers as $stockNumber){
+
+foreach ($stockNumbers as $stockNumber) {
     $urls = [
-        'http://jsjustweb.jihsun.com.tw/z/zc/zcp/zcpa/zcpa0.djhtm?b=Y&a='.$stockNumber, //資產負債年表
+        'http://jsjustweb.jihsun.com.tw/z/zc/zcp/zcpa/zcpa0.djhtm?b=Y&a=' . $stockNumber, //資產負債年表
         'http://jsjustweb.jihsun.com.tw/z/zc/zcp/zcpa/zcpa0.djhtm?b=Q&a='.$stockNumber, //資產負債季表
         'http://jsjustweb.jihsun.com.tw/z/zc/zcq/zcq0.djhtm?b=Y&a='.$stockNumber, //損益年表
         'http://jsjustweb.jihsun.com.tw/z/zc/zcq/zcq0.djhtm?b=Q&a='.$stockNumber, //損益季表
@@ -13,14 +14,21 @@ foreach ($stockNumbers as $stockNumber){
         'http://jsjustweb.jihsun.com.tw/z/zc/zc30.djhtm?b=Q&a='.$stockNumber //現金流量季表
     ];
 
+
     foreach ($urls as $url) {
         $html = $curl::initCurl($url);
+        $i = 0;
         foreach ($html->find('tr') as $a) {
-            echo($a->plaintext." ");
-            echo "\n";
+            if ($i > 2) {
+                $arr[$i] = $a->plaintext;
+            }
+            $i++;
         }
-
-
+        foreach ($arr as $a) {
+            echo $a . "\n";
+        }
     }
+
+    die();
 }
 
